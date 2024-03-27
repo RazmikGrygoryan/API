5ed11fabe9f69e12b87415670137ce952e99e377
RazmikGrygoryan <razmik97@list.ru>
2024-03-27 11:00:49 +0300

    refactor project

--- endpoints/get_one_meme.py
+++ endpoints/get_one_meme.py
@@ -5,11 +5,15 @@ from endpoints.endpoint import EndPoint
 
 
 class GetOneMemes(EndPoint):
+    text = None
+
     @allure.step('Get one meme')
     def get_one_memes(self, meme_id, return_text=False, headers=None):
         headers = headers if headers else self.headers
         self.response = requests.get(f'{self.url}/meme/{meme_id}', headers=headers)
         if return_text:
-            return self.response.text
+            self.text = self.response.text
+            return self.text
         else:
-            return self.response.json()
+            self.json = self.response.json()
+            return self.json
--- tests/test_memes.py
+++ tests/test_memes.py
@@ -16,12 +16,13 @@ def test_post_meme(create_meme, auth_token):
     }
     create_meme.create_new_meme(payload, headers={"Authorization": auth_token})
     create_meme.check_that_status_is_200()
-    create_meme.check_response_is_correct(payload['text'], payload['info'], payload['tags'])
+    create_meme.check_response_is_correct(payload['text'], payload['info'], payload['tags'], payload['url'])
 
 
 def test_one_meme(get_meme, auth_token, meme_id):
     get_meme.get_one_memes(meme_id, headers={"Authorization": auth_token})
     get_meme.check_that_status_is_200()
+    get_meme.check_is_id(meme_id)
 
 
 def test_update_meme(update_meme, auth_token, meme_id):
@@ -34,7 +35,7 @@ def test_update_meme(update_meme, auth_token, meme_id):
     }
     update_meme.update_meme(meme_id, payload, headers={"Authorization": auth_token})
     update_meme.check_that_status_is_200()
-    update_meme.check_response_is_correct(payload['text'], payload['info'], payload['tags'])
+    update_meme.check_response_is_correct(payload['text'], payload['info'], payload['tags'], payload['url'])
 
 
 def test_delete_meme(delete_meme, get_meme, auth_token, meme_id):
@@ -44,20 +45,35 @@ def test_delete_meme(delete_meme, get_meme, auth_token, meme_id):
     get_meme.check_page_not_found()
 
 
-def test_failed_data_post(create_meme, auth_token):
-    payload = {
-        1: 1,
-        2: 1,
-        3: "  ",
-        4: True
-    }
-    create_meme.create_new_meme(payload, True, headers={"Authorization": auth_token})
+@pytest.mark.parametrize("invalid_payload", [
+    {1: 1},
+    {2: 1},
+    {3: "  "},
+    {4: True}
+])
+def test_failed_data_post(create_meme, auth_token, invalid_payload):
+    create_meme.create_new_meme(invalid_payload, True, headers={"Authorization": auth_token})
     create_meme.check_bad_request()
 
 
 def test_with_incorrect_token(get_all_memes):
     get_all_memes.get_all_memes(True, headers={"Authorization": "123"})
     get_all_memes.check_is_unathorized()
+
+
+def test_get_all_memes_without_token(get_all_memes):
+    get_all_memes.get_all_memes(True)
+    get_all_memes.check_is_unathorized()
+
+
+def test_post_meme_without_token(create_meme):
+    create_meme.create_new_meme(True)
+    create_meme.check_is_unathorized()
+
+
+def test_update_meme_without_token(update_meme):
+    update_meme.update_meme(True)
+    update_meme.check_is_unathorized()
 
 
 def test_without_data(update_meme, auth_token, meme_id):
@@ -76,12 +92,13 @@ def test_update_stranger_meme(get_all_memes, update_meme, auth_token):
             payload = {
                 "id": meme['id'],
                 "text": "Meme111",
-                "url": "Meme",
+                "url": "Mem",
                 "tags": ["Meme111"],
                 "info": {"Meme": 77777}
             }
             update_meme.update_meme(meme['id'], payload, True, headers={"Authorization": auth_token})
             update_meme.check_is_forbidden()
+            break
 
 
 def test_delete_stranger_meme(get_all_memes, delete_meme, auth_token):
@@ -90,6 +107,7 @@ def test_delete_stranger_meme(get_all_memes, delete_meme, auth_token):
         if 'updated_by' in meme and meme.get('updated_by') != 'Razmik':
             delete_meme.delete_meme(meme['id'], headers={"Authorization": auth_token})
             delete_meme.check_is_forbidden()
+            break
 
 
 def test_delete_unexisted_meme(delete_meme, auth_token):
